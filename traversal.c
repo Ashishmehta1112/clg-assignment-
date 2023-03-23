0aafd8d076544a9480fa46838a164665d1dec623
Ashishmehta1112 <119177610+Ashishmehta1112@users.noreply.github.com>
2023-03-23 21:08:44 +0530

    Add files via upload

---
+++ traversal.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ #include<conio.h>
+
+ int  printarray(int arr[], int n)
+{ int i;
+
+printf("array elements are:");
+
+  for(i=0;i<n;i++)
+
+{ 
+
+  printf("%d", arr[i]);
+
+} printf("\n");
+
+return 0;}
+
+
+int main(){
+int n,i;
+
+printf("how much size array you want for traversal");
+scanf("%d",&n);
+int array[]={5,6,8,9,9,9,9,88,9,8};
+    
+
+printarray(array,n);
+}
